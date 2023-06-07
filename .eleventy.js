ac433ff2b8c07f25d6b8b384fed609356f383137
Jerome Kalumbu <jerome@MBP-de-Jerome.lan>
2023-06-07 09:57:59 +0200

    fixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/assets/sass/");
     eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addPassthroughCopy("./src/admin");
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
       return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
